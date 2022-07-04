dd13761b7dd2e3e86758932cf5a7e51f06147e27
naveen8801 <naveensharma10d@gmail.com>
2022-07-05 02:00:44 +0530

    filter states added

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -65,6 +65,49 @@ const useStyles = makeStyles({
   },
 });
 
+const LocationList = [
+  { key: 'Any', value: 'Any' },
+  { key: 'Delhi', value: 'Delhi' },
+  { key: 'Goa', value: 'Goa' },
+  ,
+  { key: 'Punjab', value: 'Punjab' },
+  ,
+  { key: 'Uttar Pradesh', value: 'Uttar Pradesh' },
+  { key: 'Bhopal', value: 'Bhopal' },
+];
+
+const PriceList = [
+  { key: 'Any', value: 'Any' },
+  { key: '0$-500$', value: '0$-500$' },
+  { key: '500$-1000$', value: '500$-1000$' },
+  { key: '1000$-1500$', value: '1000$-1500$' },
+  ,
+  { key: '1500$-2000$', value: '1500$-2000$' },
+  ,
+  { key: '2000$-2500$', value: '2000$-2500$' },
+];
+
+const TimeList = [
+  { key: 'Any', value: 'Any' },
+  { key: '10th July 2022', value: '10th July 2022' },
+  { key: '11th July 2022', value: '11th July 2022' },
+  { key: '12th July 2022', value: '12th July 2022' },
+  ,
+  { key: '13th July 2022', value: '13th July 2022' },
+  ,
+  { key: '14th July 2022', value: '14th July 2022' },
+  { key: '15th July 2022', value: '15th July 2022' },
+];
+
+const TypeList = [
+  { key: 'Any', value: 'Any' },
+  { key: 'Flat', value: 'Flat' },
+  { key: 'Double Story', value: 'Double Story' },
+  { key: 'Triple Story', value: 'Triple Story' },
+  ,
+  { key: 'Mension', value: 'Mension' },
+];
+
 function FilterBar(props) {
   const {
     filterType,
@@ -101,16 +144,13 @@ function FilterBar(props) {
           className={classes.select}
           labelId="demo-simple-select-standard-label"
           id="demo-simple-select-standard"
-          value={10}
-          //   onChange={handleChange}
+          value={filterLocation}
+          onChange={(e) => setFilterLocation(e.target.value)}
           label="Location"
         >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          {LocationList.map((item, i) => (
+            <MenuItem value={item.value}>{item.key}</MenuItem>
+          ))}
         </Select>
       </FormControl>
       <FormControl className={classes.form}>
@@ -135,16 +175,13 @@ function FilterBar(props) {
           className={classes.select}
           labelId="demo-simple-select-standard-label"
           id="demo-simple-select-standard"
-          value={10}
-          //   onChange={handleChange}
+          value={filterTime}
+          onChange={(e) => setFilterTime(e.target.value)}
           label="When"
         >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          {TimeList.map((item, i) => (
+            <MenuItem value={item.value}>{item.key}</MenuItem>
+          ))}
         </Select>
       </FormControl>
       <FormControl className={classes.form}>
@@ -169,16 +206,13 @@ function FilterBar(props) {
           className={classes.select}
           labelId="demo-simple-select-standard-label"
           id="demo-simple-select-standard"
-          value={10}
-          //   onChange={handleChange}
+          value={filterPrice}
+          onChange={(e) => setFilterPrice(e.target.value)}
           label="Price"
         >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          {PriceList.map((item, i) => (
+            <MenuItem value={item.value}>{item.key}</MenuItem>
+          ))}
         </Select>
       </FormControl>
       <FormControl className={classes.form}>
@@ -203,16 +237,13 @@ function FilterBar(props) {
           className={classes.select}
           labelId="demo-simple-select-standard-label"
           id="demo-simple-select-standard"
-          value={10}
-          //   onChange={handleChange}
+          value={filterType}
+          onChange={(e) => setFilterType(e.target.value)}
           label="Property Type"
         >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          {TypeList.map((item, i) => (
+            <MenuItem value={item.value}>{item.key}</MenuItem>
+          ))}
         </Select>
       </FormControl>
       <Button className={classes.btn}>Search</Button>
--- src/components/MainActivity.js
+++ src/components/MainActivity.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 import FilterBar from './FilterBar';
